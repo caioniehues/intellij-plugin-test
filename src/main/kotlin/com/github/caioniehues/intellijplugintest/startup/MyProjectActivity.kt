295b6f14ec65c6c9cfdd240fc3f8c633c41dda30
GitHub Action <action@github.com>
2025-11-19 04:00:33 +0000

    Template cleanup

--- src/main/kotlin/com/github/caioniehues/intellijplugintest/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/caioniehues/intellijplugintest/startup/MyProjectActivity.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.startup
+package com.github.caioniehues.intellijplugintest.startup
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
--- src/main/kotlin/com/github/caioniehues/intellijplugintest/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/caioniehues/intellijplugintest/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.caioniehues.intellijplugintest.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.caioniehues.intellijplugintest.MyBundle
+import com.github.caioniehues.intellijplugintest.services.MyProjectService
 import javax.swing.JButton
 
 
